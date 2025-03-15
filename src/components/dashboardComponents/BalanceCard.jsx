2d3cce9127ed791a3d0d9711625775384c526eb9
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2025-03-15 15:39:00 +0600

    sta

--- src/components/dashboardComponents/BalanceCard.jsx
+++ src/components/dashboardComponents/BalanceCard.jsx
@@ -3,6 +3,11 @@ import { WalletOutlined, BarsOutlined } from '@ant-design/icons'; // Assuming yo
 const BalanceCard = () => {
   return (
     <div className="bg-gray-100 rounded-lg p-6 shadow-md font-sans">
+     
+
+
+
+
       <h2 className="text-xl font-semibold mb-4">Total Balance</h2>
       <div className="text-4xl text-green-500 font-bold mb-6">$1,20,000</div>
 
@@ -11,7 +16,7 @@ const BalanceCard = () => {
           <BarsOutlined className="!text-white text-xl" />
         </div>
         <div>
-          <div className="flex gap-x-80  items-center">
+          <div className="flex gap-x-70  items-center">
             <div className="text-lg font-medium">Total Income</div>
             <div className="text-sm text-gray-500">(+60% Increase)</div>
           </div>
@@ -24,7 +29,7 @@ const BalanceCard = () => {
           <WalletOutlined className="!text-white text-xl" />
         </div>
         <div>
-          <div className="flex gap-x-80 items-center">
+          <div className="flex gap-x-70 items-center">
             <div className="text-lg font-medium">Total Expense</div>
             <div className="text-sm text-gray-500">(+70% Increase)</div>
           </div>
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ReservationChart from "../../components/dashboardComponents/ReservationChart";
 import IncomeCard from "../../components/dashboardComponents/IncomeCard";
 import BalanceCard from "../../components/dashboardComponents/BalanceCard";
 import DailyOrder from "../../components/dashboardComponents/DailyOrder";
 
 const Dashboard = () => {
+  const [activeTab, setActiveTab] = useState("Today");
+
+  const tabs = ["Today", "This Week", "This Month", "This Year"];
   return (
     <div>
+     
+      <div className="flex justify-end mb-5">
+      <div className="flex !space-x-4 bg-gray-100 p-2 rounded-lg">
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          className={`px-4 py-2 rounded-lg font-medium transition-all 
+            ${
+              activeTab === tab
+                ? "bg-red-600 !text-white"
+                : "text-gray-700 hover:bg-gray-200"
+            }`}
+          onClick={() => setActiveTab(tab)}
+        >
+          {tab}
+        </button>
+      ))}
+    </div>
+          
+      </div>
+
       <div className="flex gap-x-5">
         <div className="w-3/5">
           <IncomeCard />
